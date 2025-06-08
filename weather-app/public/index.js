fb4b340004da78267b86c300a92e60738c7730b7
Edward Gemadzi <edwardgemadzi@rocketmail.com>
2025-06-08 14:38:21 +0000

    code clean up.

--- weather-app/public/index.js
+++ weather-app/public/index.js
@@ -4,55 +4,55 @@ let WEATHER_API_KEY = '';
 fetch('/api/keys')
     .then(res => res.json())
     .then(keys => {
-    MAPTILER_API_KEY = keys.maptiler;
-    WEATHER_API_KEY = keys.weather;
+        MAPTILER_API_KEY = keys.maptiler;
+        WEATHER_API_KEY = keys.weather;
 
-    maptilersdk.config.apiKey = MAPTILER_API_KEY;
+        maptilersdk.config.apiKey = MAPTILER_API_KEY;
 
-    // Re-initialize the map *after* setting the API key
-    initApp();
+        // Re-initialize the map *after* setting the API key
+        initApp();
     }
 );
 
 function initApp(){
 
     const weatherLayers = {
-    "precipitation": {
-        "layer": null,
-        "value": "value",
-        "units": " mm"
-    },
-    "pressure": {
-        "layer": null,
-        "value": "value",
-        "units": " hPa"
-    },
-    "radar": {
-        "layer": null,
-        "value": "value",
-        "units": " dBZ"
-    },
-    "temperature": {
-        "layer": null,
-        "value": "value",
-        "units": "°"
-    },
-    "wind": {
-        "layer": null,
-        "value": "speedMetersPerSecond",
-        "units": " m/s"
-    }
+        "precipitation": {
+            "layer": null,
+            "value": "value",
+            "units": " mm"
+        },
+        "pressure": {
+            "layer": null,
+            "value": "value",
+            "units": " hPa"
+        },
+        "radar": {
+            "layer": null,
+            "value": "value",
+            "units": " dBZ"
+        },
+        "temperature": {
+            "layer": null,
+            "value": "value",
+            "units": "°"
+        },
+        "wind": {
+            "layer": null,
+            "value": "speedMetersPerSecond",
+            "units": " m/s"
+        }
     };
 
     const map = (window.map = new maptilersdk.Map({
-    container: 'map', // container's id or the HTML element to render the map
-    style: maptilersdk.MapStyle.BACKDROP,  // stylesheet location
-    zoom: 0,
-    center: [0, 0], // Central Africa
-    maxZoom: 11.9,
-    hash: true,
-    projectionControl: true,
-    projection: 'globe'
+        container: 'map', // container's id or the HTML element to render the map
+        style: maptilersdk.MapStyle.BACKDROP,  // stylesheet location
+        zoom: 0,
+        center: [0, 0], // Central Africa
+        maxZoom: 11.9,
+        hash: true,
+        projectionControl: true,
+        projection: 'globe'
     }));
 
     //       map._controls
@@ -72,39 +72,39 @@ function initApp(){
     let currentTime = null;
 
     timeSlider.addEventListener("input", (evt) => {
-    const weatherLayer = weatherLayers[activeLayer]?.layer;
-    if (weatherLayer) {
-        weatherLayer.setAnimationTime(parseInt(timeSlider.value / 1000));
-    }
-    });
-
-    // When clicking on the play/pause
-    playPauseButton.addEventListener("click", () => {
-    const weatherLayer = weatherLayers[activeLayer]?.layer;
-    if (weatherLayer) {
-        if (isPlaying) {
-        pauseAnimation(weatherLayer);
-        } else {
-        playAnimation(weatherLayer);
+        const weatherLayer = weatherLayers[activeLayer]?.layer;
+        if (weatherLayer) {
+            weatherLayer.setAnimationTime(parseInt(timeSlider.value / 1000));
         }
-    }
+        });
+
+        // When clicking on the play/pause
+        playPauseButton.addEventListener("click", () => {
+        const weatherLayer = weatherLayers[activeLayer]?.layer;
+        if (weatherLayer) {
+            if (isPlaying) {
+            pauseAnimation(weatherLayer);
+            } else {
+            playAnimation(weatherLayer);
+            }
+        }
     });
 
     function pauseAnimation(weatherLayer) {
-    weatherLayer.animateByFactor(0);
-    playPauseButton.innerText = "Play 3600x";
-    isPlaying = false;
+        weatherLayer.animateByFactor(0);
+        playPauseButton.innerText = "Play 3600x";
+        isPlaying = false;
     }
 
     function playAnimation(weatherLayer) {
-    weatherLayer.animateByFactor(3600);
-    playPauseButton.innerText = "Pause";
-    isPlaying = true;
+        weatherLayer.animateByFactor(3600);
+        playPauseButton.innerText = "Pause";
+        isPlaying = true;
     }
 
     map.on('load', function () {
-    map.setPaintProperty("Water", 'fill-color', "rgba(0, 0, 0, 0.4)");
-    initWeatherMap(initWeatherLayer);
+        map.setPaintProperty("Water", 'fill-color', "rgba(0, 0, 0, 0.4)");
+        initWeatherMap(initWeatherLayer);
     });
 
     map.on('mouseout', function(evt) {
@@ -115,179 +115,179 @@ function initApp(){
     });
 
     function updatePointerValue(lngLat) {
-    if (!lngLat) return;
-    pointerLngLat = lngLat;
-    const weatherLayer = weatherLayers[activeLayer]?.layer;
-    const weatherLayerValue = weatherLayers[activeLayer]?.value;
-    const weatherLayerUnits = weatherLayers[activeLayer]?.units;
-    if (weatherLayer) {
-        const value = weatherLayer.pickAt(lngLat.lng, lngLat.lat);
-        if (!value) {
-        pointerDataDiv.innerText = "";
-        return;
+        if (!lngLat) return;
+        pointerLngLat = lngLat;
+        const weatherLayer = weatherLayers[activeLayer]?.layer;
+        const weatherLayerValue = weatherLayers[activeLayer]?.value;
+        const weatherLayerUnits = weatherLayers[activeLayer]?.units;
+        if (weatherLayer) {
+            const value = weatherLayer.pickAt(lngLat.lng, lngLat.lat);
+            if (!value) {
+                pointerDataDiv.innerText = "";
+                return;
+            }
+            pointerDataDiv.innerText = `${value[weatherLayerValue].toFixed(1)}${weatherLayerUnits}`
         }
-        pointerDataDiv.innerText = `${value[weatherLayerValue].toFixed(1)}${weatherLayerUnits}`
-    }
     }
 
     map.on('mousemove', (e) => {
-    updatePointerValue(e.lngLat);
+        updatePointerValue(e.lngLat);
     });
 
     document.getElementById('buttons').addEventListener('click', function (event) {
-    // Change layer based on button id
-    changeWeatherLayer(event.target.id);
+        // Change layer based on button id
+        changeWeatherLayer(event.target.id);
     });
 
     function changeWeatherLayer(type) {
-    if (type !== activeLayer) {
-        if (map.getLayer(activeLayer)) {
-        const activeWeatherLayer = weatherLayers[activeLayer]?.layer;
-        if (activeWeatherLayer) {
-            currentTime = activeWeatherLayer.getAnimationTime();
-            map.setLayoutProperty(activeLayer, 'visibility', 'none');
+        if (type !== activeLayer) {
+            if (map.getLayer(activeLayer)) {
+                const activeWeatherLayer = weatherLayers[activeLayer]?.layer;
+            if (activeWeatherLayer) {
+                currentTime = activeWeatherLayer.getAnimationTime();
+                map.setLayoutProperty(activeLayer, 'visibility', 'none');
+            }
+            }
+            activeLayer = type;
+            const weatherLayer = weatherLayers[activeLayer].layer || createWeatherLayer(activeLayer);
+            if (map.getLayer(activeLayer)) {
+                map.setLayoutProperty(activeLayer, 'visibility', 'visible');
+            } else {
+                map.addLayer(weatherLayer, 'Water');
+            }
+            changeLayerLabel(activeLayer);
+            activateButton(activeLayer);
+            changeLayerAnimation(weatherLayer);
+            return weatherLayer;
         }
-        }
-        activeLayer = type;
-        const weatherLayer = weatherLayers[activeLayer].layer || createWeatherLayer(activeLayer);
-        if (map.getLayer(activeLayer)) {
-        map.setLayoutProperty(activeLayer, 'visibility', 'visible');
-        } else {
-        map.addLayer(weatherLayer, 'Water');
-        }
-        changeLayerLabel(activeLayer);
-        activateButton(activeLayer);
-        changeLayerAnimation(weatherLayer);
-        return weatherLayer;
-    }
     }
 
     function activateButton(activeLayer) {
-    const buttons = document.getElementsByClassName('button');
-    for (let i = 0; i < buttons.length; i++) {
-        const btn = buttons[i];
-        if (btn.id === activeLayer) {
-        btn.classList.add('active');
-        } else {
-        btn.classList.remove('active');
+        const buttons = document.getElementsByClassName('button');
+        for (let i = 0; i < buttons.length; i++) {
+            const btn = buttons[i];
+            if (btn.id === activeLayer) {
+                btn.classList.add('active');
+            } else {
+                btn.classList.remove('active');
+            }
         }
-    }
     }
 
     function changeLayerAnimation(weatherLayer) {
-    weatherLayer.setAnimationTime(parseInt(timeSlider.value / 1000));
-    if (isPlaying) {
-        playAnimation(weatherLayer);
-    } else {
-        pauseAnimation(weatherLayer);
-    }
+        weatherLayer.setAnimationTime(parseInt(timeSlider.value / 1000));
+        if (isPlaying) {
+            playAnimation(weatherLayer);
+        } else {
+            pauseAnimation(weatherLayer);
+        }
     }
 
     function createWeatherLayer(type){
-    let weatherLayer = null;
-    switch (type) {
-        case 'precipitation':
-        weatherLayer = new maptilerweather.PrecipitationLayer({id: 'precipitation'});
-        break;
-        case 'pressure':
-        weatherLayer = new maptilerweather.PressureLayer({
-            opacity: 0.8,
-            id: 'pressure'
-        });
-        break;
-        case 'radar':
-        weatherLayer = new maptilerweather.RadarLayer({
-            opacity: 0.8,
-            id: 'radar'
-        });
-        break;
-        case 'temperature':
-        weatherLayer = new maptilerweather.TemperatureLayer({
-            colorramp: maptilerweather.ColorRamp.builtin.TEMPERATURE_3,
-            id: 'temperature'
-        });
-        break;
-        case 'wind':
-        weatherLayer = new maptilerweather.WindLayer({id: 'wind'});
-        break;
-    }
-
-    // Called when the animation is progressing
-    weatherLayer.on("tick", event => {
-        refreshTime();
-        updatePointerValue(pointerLngLat);
-    });
-
-    // Called when the time is manually set
-    weatherLayer.on("animationTimeSet", event => {
-        refreshTime();
-    });
-
-    // Event called when all the datasource for the next days are added and ready.
-    // From now on, the layer nows the start and end dates.
-    weatherLayer.on("sourceReady", event => {
-        const startDate = weatherLayer.getAnimationStartDate();
-        const endDate = weatherLayer.getAnimationEndDate();
-        if (timeSlider.min > 0){
-        weatherLayer.setAnimationTime(currentTime);
-        changeLayerAnimation(weatherLayer);
-        } else {
-        const currentDate = weatherLayer.getAnimationTimeDate();
-        timeSlider.min = +startDate;
-        timeSlider.max = +endDate;
-        timeSlider.value = +currentDate;
+        let weatherLayer = null;
+        switch (type) {
+            case 'precipitation':
+            weatherLayer = new maptilerweather.PrecipitationLayer({id: 'precipitation'});
+            break;
+            case 'pressure':
+            weatherLayer = new maptilerweather.PressureLayer({
+                opacity: 0.8,
+                id: 'pressure'
+            });
+            break;
+            case 'radar':
+            weatherLayer = new maptilerweather.RadarLayer({
+                opacity: 0.8,
+                id: 'radar'
+            });
+            break;
+            case 'temperature':
+            weatherLayer = new maptilerweather.TemperatureLayer({
+                colorramp: maptilerweather.ColorRamp.builtin.TEMPERATURE_3,
+                id: 'temperature'
+            });
+            break;
+            case 'wind':
+            weatherLayer = new maptilerweather.WindLayer({id: 'wind'});
+            break;
         }
-    });
 
-    weatherLayers[type].layer = weatherLayer;
-    return weatherLayer;
+        // Called when the animation is progressing
+        weatherLayer.on("tick", event => {
+            refreshTime();
+            updatePointerValue(pointerLngLat);
+        });
+
+        // Called when the time is manually set
+        weatherLayer.on("animationTimeSet", event => {
+            refreshTime();
+        });
+
+        // Event called when all the datasource for the next days are added and ready.
+        // From now on, the layer nows the start and end dates.
+        weatherLayer.on("sourceReady", event => {
+            const startDate = weatherLayer.getAnimationStartDate();
+            const endDate = weatherLayer.getAnimationEndDate();
+            if (timeSlider.min > 0){
+                weatherLayer.setAnimationTime(currentTime);
+                changeLayerAnimation(weatherLayer);
+            } else {
+                const currentDate = weatherLayer.getAnimationTimeDate();
+                timeSlider.min = +startDate;
+                timeSlider.max = +endDate;
+                timeSlider.value = +currentDate;
+            }
+        });
+
+        weatherLayers[type].layer = weatherLayer;
+        return weatherLayer;
     }
 
     // Update the date time display
     function refreshTime() {
-    const weatherLayer = weatherLayers[activeLayer]?.layer;
-    if (weatherLayer) {
-        const d = weatherLayer.getAnimationTimeDate();
-        timeTextDiv.innerText = d.toString();
-        timeSlider.value = +d;
-    }
+        const weatherLayer = weatherLayers[activeLayer]?.layer;
+        if (weatherLayer) {
+            const d = weatherLayer.getAnimationTimeDate();
+            timeTextDiv.innerText = d.toString();
+            timeSlider.value = +d;
+        }
     }
 
     function changeLayerLabel(type) {
-    document.getElementById("variable-name").innerText = type;
+        document.getElementById("variable-name").innerText = type;
     }
 
     function initWeatherMap(type) {
-    const weatherLayer = changeWeatherLayer(type);
+        const weatherLayer = changeWeatherLayer(type);
     }
 
     async function searchCityWeather() {
-    const city = document.getElementById('citySearch').value.trim();
-    if (!city) {
-        alert('Please enter a city name.');
-        return;
-    }
+        const city = document.getElementById('citySearch').value.trim();
+        if (!city) {
+            alert('Please enter a city name.');
+            return;
+        }
 
-    const url = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${city}&aqi=no`;
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        if (data.error) throw new Error(data.error.message);
+        const url = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${city}&aqi=no`;
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            if (data.error) throw new Error(data.error.message);
 
-    //   const output = `${data.location.name}, ${data.location.country}<br><img src="https:${data.current.condition.icon}" alt="${data.current.condition.text}" style="width: 24px; vertical-align: middle;"> ${data.current.temp_c}°C, ${data.current.condition.text}`;
-        document.getElementById('current-location').innerText = `${data.location.name}, ${data.location.country}`;
-        document.getElementById('current-condition').innerHTML = `<img src="https:${data.current.condition.icon}" alt="${data.current.condition.text}" style="width: 24px; vertical-align: middle;"> ${data.current.temp_c}°C, ${data.current.condition.text}`;
-        document.getElementById('citySearch').value = '';
+        //   const output = `${data.location.name}, ${data.location.country}<br><img src="https:${data.current.condition.icon}" alt="${data.current.condition.text}" style="width: 24px; vertical-align: middle;"> ${data.current.temp_c}°C, ${data.current.condition.text}`;
+            document.getElementById('current-location').innerText = `${data.location.name}, ${data.location.country}`;
+            document.getElementById('current-condition').innerHTML = `<img src="https:${data.current.condition.icon}" alt="${data.current.condition.text}" style="width: 24px; vertical-align: middle;"> ${data.current.temp_c}°C, ${data.current.condition.text}`;
+            document.getElementById('citySearch').value = '';
 
-        // Center map on search result
-        centerMapOnLocation(data.location.lon, data.location.lat);
-        await getForecast(data.location.name);
-        const currentSummary = document.getElementById('current-summary');
-        currentSummary.style.display = 'block';
-        currentSummary.style.zIndex = '3';       
-    } catch (error) {
-        document.getElementById('searchResult').innerText = error.message;
-    }
+            // Center map on search result
+            centerMapOnLocation(data.location.lon, data.location.lat);
+            await getForecast(data.location.name);
+            const currentSummary = document.getElementById('current-summary');
+            currentSummary.style.display = 'block';
+            currentSummary.style.zIndex = '3';       
+        } catch (error) {
+            document.getElementById('searchResult').innerText = error.message;
+        }
     }
 
     async function getForecast(city) {
